2e92b224f0938bb2f25d00c658d0479f88694186
James McNair <jmcnair@mac602.local>
2017-09-25 13:49:32 -0400

    Initial Commit

---
+++ Archive_Candidates/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Archive_Candidates
+//
+//  Created by James McNair on 2017-09-25.
+//  Copyright © 2017 James McNair. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Archive_Candidates/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Archive_Candidates
+//
+//  Created by James McNair on 2017-09-25.
+//  Copyright © 2017 James McNair. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
